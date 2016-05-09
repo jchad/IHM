85542f541d428e1d7f7b8ca39c4c888f44958ea7
Jordan CHADUIRON <jordan.chaduiron@etu.univ-lyon1.fr>
2016-05-09 17:48:54 +0200

    Début des test tp1 part 1

---
+++ TP1P1/test/tp1p1/TP1P1Test.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp1p1;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Jordan
+ */
+public class TP1P1Test {
+    
+    public TP1P1Test() {
+    }
+
+    /**
+     * Test of main method, of class TP1P1.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        TP1P1.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
---
+++ TP1P1/test/tp1p1/Tp1p1Suite.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp1p1;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author Jordan
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({tp1p1.ConteneurTest.class, tp1p1.TP1P1Test.class})
+public class Tp1p1Suite {
+    
+}
